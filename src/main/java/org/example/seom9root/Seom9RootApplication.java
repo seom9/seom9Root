059e0c4f482e5562fc7dafc2684b6851388f376a
seom9 <dmssid78@hanmail.com>
2025-03-06 20:25:07 +0900

    Initial commit

---
+++ src/main/java/org/example/seom9root/Seom9RootApplication.java
@@ -0,0 +1,13 @@
+package org.example.seom9root;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seom9RootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Seom9RootApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/seom9root/ServletInitializer.java
@@ -0,0 +1,13 @@
+package org.example.seom9root;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Seom9RootApplication.class);
+    }
+
+}
