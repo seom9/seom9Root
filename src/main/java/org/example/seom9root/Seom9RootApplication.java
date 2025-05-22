12ec883d223841060c1854ec51586f6a506dcd80
seom9 <dmssid78@hanmail.net>
2025-05-22 21:34:49 +0900

    20250522 Board 구성

--- src/main/java/org/example/seom9root/Seom9RootApplication.java
+++ src/main/java/org/example/seom9root/Seom9RootApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Seom9RootApplication {
-    //
     public static void main(String[] args) {
+        // seom9 push
         SpringApplication.run(Seom9RootApplication.class, args);
     }
 }
