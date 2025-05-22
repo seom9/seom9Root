079f0e5a684249b8ed0f82aab70a2cf1fdf36e59
seom9 <dmssid78@hanmail.com>
2025-05-22 21:22:15 +0900

    20250522 Board 구성

---
+++ src/test/java/org/example/seom9root/Seom9RootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.seom9root;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Seom9RootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
